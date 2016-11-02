bf57af389c359f18ce63ee3351d528a3a6d9516f
Tech Jene <1785529047@qq.com>
2016-11-02 22:14:20 +0800

    增加后天自动更新功能，目前switchCity仍然存在问题。

--- src/com/weatherforecast/app/activity/WeatherActivity.java
+++ src/com/weatherforecast/app/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.weatherforecast.app.R;
+import com.weatherforecast.app.service.AutoUpdateService;
 import com.weatherforecast.app.util.HttpCallbackListener;
 import com.weatherforecast.app.util.HttpUtil;
 import com.weatherforecast.app.util.Utility;
@@ -188,5 +189,7 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 }
